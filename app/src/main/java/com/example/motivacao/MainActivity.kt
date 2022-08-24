11bef36ab418ff0a422cc11a6cf340baa397ce07
Wiilames <willmiranda.94@gmail.com>
2022-08-24 13:11:00 -0400

    Primeiro activity Layout sen estruturado

--- app/src/main/java/com/example/motivacao/MainActivity.kt
+++ app/src/main/java/com/example/motivacao/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.motivacao
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.motivacao.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var  binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        //esconder barra de navegação
+        supportActionBar?.hide()
+
+        //eventos
+        binding.buttonNewPhrase.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if(view.id == R.id.button_new_phrase){
+            var s = ""
+        }
     }
 }
